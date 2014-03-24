df7c5eb9d402d8598bbeb96cf90df465637d2e2b
Przemyslaw Koziel <mos.koziel@gmail.com>
2014-03-24 12:13:06 +0100

    Clojurescript added to the project

--- project.clj
+++ project.clj
@@ -2,10 +2,22 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2173"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.5"]
                  [ring-server "0.3.1"]]
-  :plugins [[lein-ring "0.8.10"]]
+  :plugins [[lein-ring "0.8.10"]
+            [lein-cljsbuild "1.0.2"]]
+  :cljsbuild {
+               :builds [{
+                          ; The path to the top-level ClojureScript source directory:
+                          :source-paths ["src-cljs"]
+                          ; The standard ClojureScript compiler options:
+                          ; (See the ClojureScript compiler documentation for details.)
+                          :compiler {
+                                      :output-to "resources/public/js/main.js"
+                                      :optimizations :whitespace
+                                      :pretty-print true}}]}
   :ring {:handler webpage.handler/app
          :init webpage.handler/init
          :destroy webpage.handler/destroy}
--- src/webpage/routes/home.clj
+++ src/webpage/routes/home.clj
@@ -7,7 +7,9 @@
   (layout/common
     [:div#header
      [:div#home-button [:a#button {:href "/"} "Home"]]
-     [:div#about-button [:a#button {:href "/about"} "About"]]]))
+     [:div#about-button [:a#button {:href "/about"} "About"]]
+     [:script {:src "js/main.js" :type "text/javascript"}]
+     [:script "clock.main()"]]))
 
 (defroutes home-routes
   (GET "/" [] (home))
