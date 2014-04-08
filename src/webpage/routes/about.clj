49b1f1fce310a4685e1fb0c9a2b3dc70a9a8ce1b
Przemyslaw Koziel <mos.koziel@gmail.com>
2014-04-08 21:14:20 +0200

    About page filled in.

--- src/webpage/routes/about.clj
+++ src/webpage/routes/about.clj
@@ -5,7 +5,19 @@
 
 (defn about []
   (layout/common
-    [:div#header
+    [:div#left
      (menu/vertical-menu)]
-    [:div#main-about
-      [:ul [:li [:a#link-text {:href "http://clojure.org/"} "This webpage was developed in clojure."]]]]))
+    [:div#right
+      [:div#about-text
+        [:p "This blog/webpage was developed with using "
+         [:a#link-text {:href "http://clojure.org/"} "clojure"]
+         " at the server side and "
+         [:a#link-text {:href "http://clojure.org/clojurescript"} "clojurescript"]
+         " at the client side."]
+        [:p "Menu is a variation of "
+          [:a#link-text {:href "http://demo.tutorialzine.com/2010/06/css3-minimalistic-navigation-menu/demo.html"} "css3"]
+          " demo. Homepage binary clock was inspired by "
+          [:a#link-text {:href "http://holmsand.github.io/reagent/news/binary-clock.html"} "holmsand"]
+          ". Repository with the source code you can be found on "
+          [:a#link-text {:href "https://github.com/randspy/webpage"} "github"]
+          "."]]]))
