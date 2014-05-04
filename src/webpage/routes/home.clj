0a294e93ab846b4f3ccf5ac2d75c8bad3736ab4b
Przemyslaw Koziel <mos.koziel@gmail.com>
2014-05-04 16:31:41 +0200

    Conditons for IE added

--- src/webpage/routes/home.clj
+++ src/webpage/routes/home.clj
@@ -12,9 +12,17 @@
       (menu/vertical-menu)]
      [:div#right
       [:div#clock
+       "<!--[if lte IE 8]> Internet Exploler 8 and balow are not supported.<![endif]-->"
+       "<![if gt IE 8]-->"
        [:script {:src "js/main.js" :type "text/javascript"}]
        [:script {:src "http://fb.me/react-0.9.0.js"}]
-       [:script "clock.core.run(\"clock\")"]]]]))
+       [:script "clock.core.run(\"clock\")"]
+       "<![endif]-->"
+       "<!--[if !IE]>"
+       [:script {:src "js/main.js" :type "text/javascript"}]
+       [:script {:src "http://fb.me/react-0.9.0.js"}]
+       [:script "clock.core.run(\"clock\")"]
+       "<![endif]-->"]]]))
 
 (defroutes home-routes
   (GET "/" [] (home))
